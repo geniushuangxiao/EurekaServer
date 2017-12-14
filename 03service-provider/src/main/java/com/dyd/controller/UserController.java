1f2cca351a4bc3904d62f3a354ff1ebfd75fb3c6
geniushuangxiao <geniushuangxiao@163.com>
2017-12-15 00:14:06 +0800

    修改数据操作

--- 03service-provider/src/main/java/com/dyd/controller/UserController.java
+++ 03service-provider/src/main/java/com/dyd/controller/UserController.java
@@ -2,21 +2,36 @@ package com.dyd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dyd.entity.bean.User;
 import com.dyd.entity.dao.UserRepository;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
 	@GetMapping("/add")
-	private @ResponseBody String add() {
+	public @ResponseBody String add() {
+		// 增
 		User user = new User("Bob", "666@hotmail.com");
 		userRepository.save(user);
+
+		// 查
+		Iterable<User> all = userRepository.findAll();
+
+		// 改
+		for (User u : all) {
+			u.setEmail("999@hotmail.com");
+			userRepository.save(u);
+		}
+
+		// 删
+		userRepository.delete(user);
 		return "Success";
 	}
 
