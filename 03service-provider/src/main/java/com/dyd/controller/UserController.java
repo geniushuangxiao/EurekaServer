11024d60a5554bcd4c28d939d3b7a4773a38f3cb
geniushuangxiao <geniushuangxiao@163.com>
2017-12-22 23:03:50 +0800

    修复若干问题，使用swagger管理api

--- 03service-provider/src/main/java/com/dyd/controller/UserController.java
+++ 03service-provider/src/main/java/com/dyd/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.dyd.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dyd.entity.bean.User;
 import com.dyd.entity.dao.UserRepository;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/user")
@@ -15,29 +25,36 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@GetMapping("/add")
-	public @ResponseBody String add() {
-		// 增
-		User user = new User("Bob", "666@hotmail.com");
+	@ApiOperation("添加用户")
+	@PostMapping("/add")
+	public @ResponseBody String add(@RequestBody User user) {
 		userRepository.save(user);
-
-		// 查
-		Iterable<User> all = userRepository.findAll();
-
-		// 改
-		for (User u : all) {
-			u.setEmail("999@hotmail.com");
-			userRepository.save(u);
-		}
-
-		// 删
-		userRepository.delete(user);
 		return "Success";
 	}
 
+	@ApiOperation("修改用户")
+	@PostMapping("modify")
+	private @ResponseBody String modify(@RequestBody User user) {
+		userRepository.save(user);
+		return "Success";
+	}
+
+	@ApiOperation("删除用户")
+	@DeleteMapping("/delete")
+	public @ResponseBody String delete(@RequestParam long id) {
+		userRepository.delete(id);
+		return "Success";
+	}
+
+	@ApiOperation("查询所有用户")
 	@GetMapping("/all")
-	public @ResponseBody Iterable<User> all() {
-		return userRepository.findAll();
+	public @ResponseBody List<User> all() {
+		Iterator<User> iterator = userRepository.findAll().iterator();
+		List<User> users = new ArrayList<User>();
+		while (iterator.hasNext()) {
+			users.add(iterator.next());
+		}
+		return users;
 	}
 
 }
--- 03service-provider/src/main/java/com/dyd/entity/bean/User.java
+++ 03service-provider/src/main/java/com/dyd/entity/bean/User.java
@@ -5,17 +5,31 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	@Setter
 	private long id;
 	@NonNull
+	@Getter
+	@Setter
 	private String name;
 	@NonNull
+	@Getter
+	@Setter
 	private String email;
 }
